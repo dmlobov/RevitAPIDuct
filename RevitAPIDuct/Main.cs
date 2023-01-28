7ead423b5708093d3b75100626566aa5aa8d9390
dmlob <dmlob@DESKTOP-0VEH751>
2023-01-28 12:53:10 +0300

    Добавьте файлы проекта.

---
+++ RevitAPIDuct/Main.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIDuct
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var ducts = new FilteredElementCollector(doc)
+                 .OfClass(typeof(Duct))
+                 .Cast<Duct>()
+                 .ToList();
+
+
+            TaskDialog.Show("Duct count", ducts.Count.ToString());
+
+            return Result.Succeeded;
+        }
+    }
+}
